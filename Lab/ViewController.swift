5ef459e73a5baacfedd79147c091dfee4065bca4
Lenayah <lfanesie@gmail.com>
2024-01-05 13:40:55 -0500

    update

--- Lab/ViewController.swift
+++ Lab/ViewController.swift
@@ -12,26 +12,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var username: UITextField!
     
     
+    @IBOutlet weak var forgotUsername: UIButton!
+
+
+    @IBOutlet weak var forgotPassword: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func forgotPassword(_ sender: UIButton) {
-        performSegue(withIdentifier: "Segway1", sender: sender)
-    }
-    @IBAction func forgotUserName(_ sender: UIButton) {
-        performSegue(withIdentifier: "Segway1", sender: sender)
+    
+    @IBAction func forgotP(_ sender: UIButton) {
+        performSegue(withIdentifier: "segway1", sender: sender)
     }
     
+    @IBAction func forgotU(_ sender: UIButton) {
+        performSegue(withIdentifier: "segway1", sender: sender)
+        
+    }
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if sender == forgotPasswordButton {
-            segue.destination.navigationItem.title = “Forgot Password”
-        } else if sender == forgotUsernameButton {
-            segue.destination.navigationItem.title = “Forgot Username”
+        
+        
+        guard let sender = sender as? UIButton else {return}
+        
+        if sender == forgotPassword {
+            segue.destination.navigationItem.title = "Forgot Password"
+        }
+        else if sender == forgotUsername {
+            segue.destination.navigationItem.title = "Forgot Username"
         } else {
-            segue.destination.navigationItem.title = "welcome \(userNameText.text ?? "user")"
-
+            segue.destination.navigationItem.title = "welcome \(username.text ?? "user")"
+            
+        }
+        
     }
     
     
